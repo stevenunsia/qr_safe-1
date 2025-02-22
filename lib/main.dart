45c009acccde6997e3923deba80bcfeec51d2672
steven04@unsia.ac.id <steven04@student.unsia.ac.id>
2025-02-22 17:08:16 +0700

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:qr_code_scanner/qr_code_scanner.dart';
 import 'package:http/http.dart' as http;
+import 'package:url_launcher/url_launcher.dart';
 import 'dart:convert';
+import 'package:logger/logger.dart';
 
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -14,13 +16,12 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(primarySwatch: Colors.blueGrey),
       home: const QRViewExample(),
-      
     );
   }
 }
 
 class QRViewExample extends StatefulWidget {
-  const QRViewExample({Key? key}) : super(key: key);
+  const QRViewExample({super.key});
 
   @override
   State<StatefulWidget> createState() => _QRViewExampleState();
@@ -30,6 +31,7 @@ class _QRViewExampleState extends State<QRViewExample> {
   final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
   Barcode? result;
   QRViewController? controller;
+  final logger = Logger();
 
   @override
   void reassemble() {
@@ -94,46 +96,86 @@ class _QRViewExampleState extends State<QRViewExample> {
   }
 
   Future<void> _checkWithVirusTotal(String url) async {
-  const apiKey = 'YOUR_API_KEY';
-  final encodedUrl = base64Url.encode(utf8.encode(url)).replaceAll('=', '');  // Encode and remove padding
-  final apiUrl = 'https://www.virustotal.com/api/v3/urls/$encodedUrl';
+    const apiKey = '72a07f99c4b045ae9de25307f755973fbe40d7ccaf4625ba6fdb6b14590c60af';
+    final encodedUrl = base64Url.encode(utf8.encode(url)).replaceAll('=', '');
+    final apiUrl = 'https://www.virustotal.com/api/v3/urls/$encodedUrl';
 
-  try {
-    final response = await http.get(
-      Uri.parse(apiUrl),
-      headers: {
-        'x-apikey': apiKey,
-        'Content-Type': 'application/json',
-      },
-    );
+    logger.d('Checking URL: $apiUrl'); // Debugging line
 
-    if (response.statusCode == 200) {
-      final jsonResponse = json.decode(response.body);
-      // Tampilkan hasil dari VirusTotal (misalnya dideteksi atau aman)
-      final scanResult = jsonResponse['data']['attributes']['last_analysis_stats'];
-      showDialog(
-        context: context,
-        builder: (_) => AlertDialog(
-          title: const Text('VirusTotal Result'),
-          content: Text('Scan Result: ${scanResult.toString()}'),
-          actions: <Widget>[
-            TextButton(
-              child: const Text('OK'),
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-            ),
-          ],
-        ),
+    try {
+      final response = await http.get(
+        Uri.parse(apiUrl),
+        headers: {
+          'x-apikey': apiKey,
+          'Content-Type': 'application/json',
+        },
       );
-    } else {
-      _showErrorDialog('Error: Unable to scan the URL with VirusTotal.');
-    }
-  } catch (e) {
-    _showErrorDialog('Error: $e');
-  }
-}
 
+      logger.d('Response status: ${response.statusCode}'); // Debugging line
+      logger.d('Response body: ${response.body}'); // Debugging line
+
+      if (response.statusCode == 200) {
+        final jsonResponse = json.decode(response.body);
+        final scanResult = jsonResponse['data']['attributes']['last_analysis_stats'];
+
+        int malicious = scanResult['malicious'] ?? 0;
+        int suspicious = scanResult['suspicious'] ?? 0;
+
+        if (malicious > 0 || suspicious > 0) {
+          _showWarningDialog(url);
+        } else {
+          _showSafeDialog(url);
+        }
+      } else {
+        _showErrorDialog('Error: Unable to scan the URL with VirusTotal.');
+      }
+    } catch (e) {
+      _showErrorDialog('Error: $e');
+    }
+  }
+
+  void _showWarningDialog(String url) {
+    showDialog(
+      context: context,
+      builder: (_) => AlertDialog(
+        title: const Text('URL Berbahaya'),
+        content: Text('Website ini terdeteksi berbahaya atau mencurigakan.\n\n$url'),
+        actions: <Widget>[
+          TextButton(
+            child: const Text('OK'),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _showSafeDialog(String url) {
+    showDialog(
+      context: context,
+      builder: (_) => AlertDialog(
+        title: const Text('Website Aman'),
+        content: Text('Website ini aman untuk dikunjungi.\n\n$url'),
+        actions: <Widget>[
+          TextButton(
+            child: const Text('Buka di Browser'),
+            onPressed: () {
+              Navigator.of(context).pop();
+              _launchURL(url);
+            },
+          ),
+          TextButton(
+            child: const Text('Tutup'),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+        ],
+      ),
+    );
+  }
 
   void _showErrorDialog(String message) {
     showDialog(
@@ -151,6 +193,15 @@ class _QRViewExampleState extends State<QRViewExample> {
         ],
       ),
     );
+  }
+
+  Future<void> _launchURL(String url) async {
+    final Uri uri = Uri.parse(url);
+    if (await canLaunchUrl(uri)) {
+      await launchUrl(uri);
+    } else {
+      _showErrorDialog('Could not open the URL.');
+    }
   }
 
   @override
